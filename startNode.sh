47a8ef2b0d43a22bb07ed7478e26441370dfb183
root <root@ip-172-31-22-116.us-west-2.compute.internal>
2016-04-26 20:58:35 +0000

    node startup for private net

---
+++ startNode.sh
@@ -0,0 +1,3 @@
+geth --nodiscover --genesis /mnt/persistent/ethereum/genomeNet/customGenesis.json --datadir /mnt/persistent/ethereum/genomeNet --identity "ucscMainNode" --networkid 93324 --autodag --nat "any" --unlock 0 console
+echo boot node is 'enode://68c961cdd20d02b715b94420b45960fff3a5573c1ef7b4f1129161e2d36a90c24e9320b3e484021104fc223d5a106eaaac138ba117cd2e4c52af6c78a0785eb5@[::]:30303?discport=0'
+echo see https://souptacular.gitbooks.io/ethereum-tutorials-and-tips-by-hudson/content/private-chain.html
